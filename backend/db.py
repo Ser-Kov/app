456047e44e55297dbbc5df77bf1f1806116325d8
Cергей Ковалевский <ser.kovalevskiy@gmail.com>
2024-08-26 16:21:43 +0300

    Домашнее задание по теме "Модели SQLALchemy. Отношения между таблицами".

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import declarative_base
 
 engine = create_engine('sqlite:///taskmanager.db', echo=True)
 
-
+Base = declarative_base()
 
 
 
